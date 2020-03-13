a06653d8993c309631db3014c2d4493729559e3b
nafis195 <nafisbuet1@gmail.com>
2020-03-12 18:49:01 -0700

    order summary added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,15 +7,26 @@ import Product from '../Product/Product';
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+
+    const handleAddProduct = (product) => {
+        console.log('product added', product)
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
     return (
         <div className = "shop-container">
             <div className="product-container">
                 {
-                    products.map(pd => <Product product={pd}></Product>)
+                    products.map(pd => <Product product={pd}
+                        handleAddProduct = {handleAddProduct}>
+                    </Product>)
                 }
             </div>
             <div className="cart-container">
                 <h3>This is Cart</h3>
+                <h5>Order Summary: {cart.length}</h5>
             </div>
         </div>
     );
