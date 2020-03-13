4d96374e38c85d97af68da03a82c35543667c988
nafis195 <nafisbuet1@gmail.com>
2020-03-12 20:31:06 -0700

    added all cart calculation

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import fakeData from '../../fakeData';
 import './Shop.css';
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
@@ -25,8 +26,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h3>This is Cart</h3>
-                <h5>Order Summary: {cart.length}</h5>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
