f348c48d2820ce6f256d9db2cf3de0c161e1e2fe
nafis195 <nafisbuet1@gmail.com>
2020-03-15 21:56:33 -0700

    added route switch

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
             <nav>
                 <a href="/Shop">Shop</a>
                 <a href="/Review">Order Review</a>
-                <a href="/Manage">Manage Inventory</a>
+                <a href="/inventory">Manage Inventory</a>
             </nav>
         </div>
     );
