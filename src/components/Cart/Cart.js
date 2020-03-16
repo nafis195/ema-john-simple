82e3d584d3832a9a9dfe053684fa42acc4a75ac0
nafis195 <nafisbuet1@gmail.com>
2020-03-16 02:42:31 -0700

    updated the shop component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 const Cart = (props) => {
     const cart = props.cart;
-    
     //const totalPrice = cart.reduce((total, prd) => total + prd.price, 0)
     let total = 0;
     for (let i = 0; i < cart.length; i++) {
