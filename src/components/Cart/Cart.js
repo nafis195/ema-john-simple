ba3dc57129dd284a0c5f112e0b99cb0bbb1e6db6
nafis195 <nafisbuet1@gmail.com>
2020-03-16 02:29:48 -0700

    added order review component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Cart = (props) => {
     const cart = props.cart;
+    
     //const totalPrice = cart.reduce((total, prd) => total + prd.price, 0)
     let total = 0;
     for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
-        total = total + product.price;
+        total = total + product.price * product.quantity;
     }
 
     let shipping = 0;
-    if(total > 35){
+    if (total > 35) {
         shipping = 0;
     }
     else if (total > 15) {
@@ -37,6 +39,9 @@ const Cart = (props) => {
             <p>Shipping cost: {shipping}</p>
             <p>Tax: {tax}</p>
             <p>total price: {grandTotal}</p>
+            <Link to="/review">
+                <button className="main-button">Review Order</button>
+            </Link>
         </div>
     );
 };
