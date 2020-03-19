c8e5982532f6b1e1f72e3a1d8d38070ada61514d
nafis195 <nafisbuet1@gmail.com>
2020-03-18 23:26:59 -0700

    added sign-in & sign-out

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Auth from './use-auth';
 
 const Login = () => {
+    const auth = Auth();
     return (
         <div>
             <h1>Join Login</h1>
+            {
+                auth.user ? <button onClick={auth.signOut}>Sign out</button> : <button onClick={auth.signInWithGoogle}>Sign in With Google</button>
+            }
         </div>
     );
 };
